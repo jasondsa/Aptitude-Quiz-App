402dee3e62b2ba2a118118d30d1f82cc0b839797
Jason <dsajason2002@gmail.com>
2024-05-21 21:16:13 +0530

    Removed Some Files and Added DatabaseHelper File

---
+++ app/src/main/java/com/azamovhudstc/quizapp/database/DatabaseHelper.java
@@ -0,0 +1,23 @@
+package com.azamovhudstc.quizapp.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    public DatabaseHelper(@Nullable Context context) {
+        super(context, "QuizApp.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
