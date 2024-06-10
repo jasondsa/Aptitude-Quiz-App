013b413296cdce42d7ea666b943c72bbf18124e9
Jason <dsajason2002@gmail.com>
2024-06-11 01:30:47 +0530

    Completed with the Android Project but didn't/won't connect to FireBase

--- app/src/main/java/com/azamovhudstc/quizapp/ui/AboutActivity.java
+++ app/src/main/java/com/azamovhudstc/quizapp/ui/AboutActivity.java
@@ -6,13 +6,15 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.Toolbar;
 
 import com.azamovhudstc.quizapp.R;
 
 public class AboutActivity extends AppCompatActivity {
     androidx.appcompat.widget.Toolbar toolbar;
-    CardView github,instagram,linkedin,telegram;
+
+    ImageView githubIcon,dailerIcon,linkedinIcon,gmailIcon;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,36 +23,37 @@ public class AboutActivity extends AppCompatActivity {
         toolbar.setNavigationOnClickListener(v->{
             onBackPressed();
         });
-        github.setOnClickListener(v->{
+        githubIcon.setOnClickListener(v->{
             String url = "https://github.com/jasondsa";
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(url));
             startActivity(i);
         });
-        instagram.setOnClickListener(v->{
-            String url = "https://www.instagram.com/gita.uzofficial/";
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(url));
-            startActivity(i);
+        dailerIcon.setOnClickListener(v->{
+            String url = "tel: +917972163039";
+            Intent intent = new Intent(Intent.ACTION_DIAL);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
         });
-        linkedin.setOnClickListener(v->{
+        linkedinIcon.setOnClickListener(v->{
             String url = "https://www.linkedin.com/in/jason-dsa/";
             Intent i = new Intent(Intent.ACTION_VIEW);
             i.setData(Uri.parse(url));
             startActivity(i);
         });
-        telegram.setOnClickListener(v->{
-            String url = "https://t.me/stc_android";
-            Intent i = new Intent(Intent.ACTION_VIEW);
-            i.setData(Uri.parse(url));
-            startActivity(i);
+        gmailIcon.setOnClickListener(v->{
+            String url = "mailto:dsajason2002@gmail.com";
+            Intent intent = new Intent(Intent.ACTION_VIEW);
+            intent.setData(Uri.parse(url));
+            startActivity(intent);
+
         });
     }
     private void initView(){
         toolbar=findViewById(R.id.toolbar2);
-        github=findViewById(R.id.github);
-        instagram=findViewById(R.id.instagram);
-        linkedin=findViewById(R.id.linkedin);
-        telegram=findViewById(R.id.cardView3);
+        githubIcon=findViewById(R.id.githubIcon);
+        dailerIcon=findViewById(R.id.dailerIcon);
+        linkedinIcon=findViewById(R.id.linkedinIcon);
+        gmailIcon=findViewById(R.id.gmailIcon);
     }
 }
